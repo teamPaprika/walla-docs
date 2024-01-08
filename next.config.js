5abefd84e7007bead80d97dc2307e30aa3575b67
RumbleBi <olzlel2000@gmail.com>
2024-01-08 16:25:59 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  async rewrites() {
-    return [
-      {
-        source: "/support",
-        destination: "https://walla.my/survey/dBYpZ05oMg5Bl1XxMGkj",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/support",
+  //       destination: "https://walla.my/survey/dBYpZ05oMg5Bl1XxMGkj",
+  //     },
+  //   ];
+  // },
 });
 
 module.exports = withNextra(
