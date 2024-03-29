57f312a9f809f59a99a4d9d17347db3f56704a1d
RumbleBi <olzlel2000@gmail.com>
2024-03-29 11:59:32 +0900

    en 리다이렉트 문제 해결

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+import nextra from "nextra";
+
+const withNextra = nextra({
+    theme: "nextra-theme-docs",
+    themeConfig: "./theme.config.tsx",
+    // staticImage: true,
+});
+
+export default withNextra({
+    reactStrictMode: true,
+    i18n: {
+        locales: ["en", "ko"],
+        defaultLocale: "ko",
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/introduction',
+                permanent: true,
+            },
+        ];
+    },
+});
